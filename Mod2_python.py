f46c226e9d96c9fd022c2f6120d1d9e0e0e4b59f
JoseDeOcampo <144511160+JoseDeOcampo@users.noreply.github.com>
2023-09-29 16:48:55 -0400

    Update Mod2_python.py

--- Mod2_python.py
+++ Mod2_python.py
@@ -1,9 +1,9 @@
 if __name__ == "__main__":
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    num3 = float(input("Enter the third number: "))
-    num4 = float(input("Enter the fourth number: "))
+    num1 = float(input("Enter the first number: ")) #This line prompts the user to input the first number
+    num2 = float(input("Enter the second number: ")) #this line prompts the user to input the second number and stores the converted floating-point value in the variable num2
+    num3 = float(input("Enter the third number: ")) #This line prompts the user to input the third number and stores the converted floating-point value in the variable num3
+    num4 = float(input("Enter the fourth number: ")) #This line prompts the user to input the fourth number and stores the converted floating-point value in the variable num4
 
-    average = (num1 + num2 + num3 + num4) / 4
+    average = (num1 + num2 + num3 + num4) / 4 #calculate average
 
-    print(f"The average of {num1}, {num2}, {num3}, and {num4} is: {average:.2f}")
+    print(f"The average of {num1}, {num2}, {num3}, and {num4} is: {average:.2f}") #print average
